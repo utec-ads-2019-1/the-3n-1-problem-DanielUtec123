d5e2d5fa94f72fc44e7ff8a1525d47eb8241f159
DanielUtec123 <34191864+DanielUtec123@users.noreply.github.com>
2019-04-02 10:49:23 -0500

    accepted solution

--- main.cpp
+++ main.cpp
@@ -2,6 +2,53 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int ciclo(int n){
+    int ciclo=0;
+    while (n>1){
+        if(n%2==1)
+        {
+            n = 3*n+1;
+            ciclo++;
+        }
+        if(n%2==0)
+        {
+            n = n/2;
+            ciclo++;
+        }
+
+    }
+    if(n==1){
+        ciclo++;
+    }
+    return ciclo;
+}
+
+int main() {
+
+    int i,j,max;
+
+    while (cin>>i>>j){
+        max=0;
+        if(i>=j){
+            for (int s=j;s<=i;s++){
+                if(max<ciclo(s)){
+                    max =ciclo(s);
+                }
+            }
+
+
+        }
+        if(i<j){
+            for (int s=i;s<=j;s++){
+                if(max<ciclo(s)){
+                    max =ciclo(s);
+                }
+            }
+        }
+        cout << i << " " << j << " " << max<<endl;
+
+    }
+
+
     return 0;
 }
